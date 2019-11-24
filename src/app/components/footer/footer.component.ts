93b6475a042a3f8b527b003d8af002f449622eae
joellion32 <joeltoribiopolanco@gmail.com>
2019-11-24 09:43:04 -0400

    final seccion 4

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 anio = new Date().getFullYear();
-  constructor() { }
+  constructor(public data: DataService) { }
 
   ngOnInit() {
   }
